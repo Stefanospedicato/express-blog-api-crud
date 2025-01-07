23fe7b3a41ca421b7d1974e7d04b56ff96220a36
Stefanospedicato <stefano.spedicato11@gmail.com>
2025-01-07 13:44:38 +0100

    feat: add methods in postsController.js

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,1 +1,29 @@
 const posts = require('../posts')
+
+const index = (req,res) => {
+  res.send('Elenco posts')
+}
+const show = (req,res) => {
+  res.send('Visualizzo un post')
+}
+const store = (req,res) => {
+  res.send('Aggiungo un post')
+}
+const update = (req,res) => {
+  res.send('Modifico un post con id' + req.params.id)
+}
+const modify = (req,res) => {
+  res.send('Modifica parziale di un post con id' + req.params.id)
+}
+const destroy = (req,res) => {
+  res.send('Elimino posts')
+}
+
+module.exports = {
+  index,
+  show, 
+  store,
+  update,
+  modify,
+  destroy
+}
