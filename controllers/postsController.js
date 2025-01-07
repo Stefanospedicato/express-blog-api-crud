05a33220d2965766817a4ac5db11f3f9cec6a8c2
Stefanospedicato <stefano.spedicato11@gmail.com>
2025-01-07 14:20:05 +0100

    feat: add show property in postsController.js

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -3,18 +3,33 @@ const posts = require('../data/posts')
 const index = (req,res) => {
   res.json(posts)
 }
+
 const show = (req,res) => {
-  res.send('Visualizzo un post')
+  const id = req.params.id;
+  const post = posts.find(post => post.id == id);
+  if(!post){
+    res.status(404)
+    return res.json({
+      message: 'Questo post non esiste',
+      status: 404,
+      error: 'Not Found'
+    })
+  }
+  res.json(post)
 }
+
 const store = (req,res) => {
   res.send('Aggiungo un post')
 }
+
 const update = (req,res) => {
   res.send('Modifico un post con id' + req.params.id)
 }
+
 const modify = (req,res) => {
   res.send('Modifica parziale di un post con id' + req.params.id)
 }
+
 const destroy = (req,res) => {
   res.send('Elimino posts')
 }
