5eb0df498edbf83a81574b9d7f5639adde851b70
Stefanospedicato <stefano.spedicato11@gmail.com>
2025-01-08 12:31:27 +0100

    feat: milestone 3 part 3 complete

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -24,9 +24,14 @@ const show = (req,res) => {
 }
 
 const store = (req,res) => {
-  console.log(req.body);
-  
-  res.send('Aggiungo un post')
+  const id = posts.at(-1).id + 1;
+  const newPost = {
+    id,
+    ...req.body
+  }
+  posts.push(newPost);
+  res.status(201);
+  res.json(posts)
 }
 
 const update = (req,res) => {
