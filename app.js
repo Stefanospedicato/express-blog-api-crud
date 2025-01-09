7ffa78beac0d0865139387cc8c314267fb73d46c
Stefanospedicato <stefano.spedicato11@gmail.com>
2025-01-09 11:53:45 +0100

    feat: add showError middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const postsRouter = require('./routers/posts')
-const notFound = require('./middlewares/notFound')
+const postsRouter = require('./routers/posts');
+const notFound = require('./middlewares/notFound');
+const showError = require('./middlewares/showError');
 
 const app = express();
 const port = 3000
@@ -11,9 +12,9 @@ app.get('/' , (req,res) => {
   res.send('Server attivo')
 })
 
-app.use('/posts' , postsRouter)
-
-app.use(notFound)
+app.use('/posts' , postsRouter);
+app.use(showError);
+app.use(notFound);
 
 app.listen(port , () => {
   console.log('Ascolto...');
